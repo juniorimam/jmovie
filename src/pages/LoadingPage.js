5f81c65e7512087ea5bf295cac646af8b36765ea
juniorimam <juniorimam123@gmail.com>
2022-10-23 14:16:58 +0700

    fix custom element

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,20 @@
+class LoadingPage extends HTMLElement {
+  constructor() {
+    super();
+
+    this.setAttribute(
+      "class",
+      "container flex flex-col justify-center items-center gap-1 min-w-full min-h-screen bg-slate-900"
+    );
+
+    this.innerHTML = `
+      <div class="flex flex-row items-center -ml-5">
+        <img src="https://img.icons8.com/material-rounded/48/000000/play--v1.png" class="invert"/>
+        <h1 class="text-4xl text-white font-bold ">JMovie</h1>
+      </div>
+      <p class="text-sm text-slate-500">Loading, please wait...</p>
+    `;
+  }
+}
+
+customElements.define("loading-page", LoadingPage);
